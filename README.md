1d502aa83ada5201caa2ed5a69de9ef818093363
JeelsBoobz <jeelsboobz@gmail.com>
2022-09-29 12:49:04 +0700

    First commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Killergram
+
+An Android Xposed module to remove sponsored messages of Telegram.
+
+Also allows you to copy or save messages when "Restrict saving content" is enabled. Forwarding messages is still not working due to server limitation.
+
+### Support clients
+
+- **Official** org.telegram.messenger
+- **Official** org.telegram.messenger.web
+- **Official** org.telegram.messenger.beta
+- **NekoX** nekox.messenger
+- **Nanogram** com.cool2645.nekolite
+- **Plus Messenger** org.telegram.plus
+- **iMe Messenger** com.iMe.android
+- **BGram** org.telegram.BifToGram
+- **Catogram** ua.itaysonlab.messenger
+- **Forkgram** org.forkclient.messenger
+- **Forkgram** org.forkclient.messenger.beta
+- **aka** org.aka.messenger
+- **TurboTel Pro** ellipi.messenger
+- **Catogram X** org.nift4.catox
+- **OwlGram** it.owlgram.android
+- **Ninjagram** me.ninjagram.messenger
+- **Telegraph** ir.ilmili.telegraph
+
+## License
+
+This project is licensed under the [GNU General Public Licence, version 3](https://choosealicense.com/licenses/gpl-3.0/).
---
+++ app/src/main/java/com/jeelsboobz/killergram/MainHook.java
@@ -0,0 +1,79 @@
+package com.jeelsboobz.killergram;
+
+import java.util.Arrays;
+import java.util.List;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodReplacement;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class MainHook implements IXposedHookLoadPackage {
+    public final static List<String> hookPackages = Arrays.asList(
+            "org.telegram.messenger",
+            "org.telegram.messenger.web",
+            "org.telegram.messenger.beta",
+            "nekox.messenger",
+            "com.cool2645.nekolite",
+            "org.telegram.plus",
+            "com.iMe.android",
+            "org.telegram.BifToGram",
+            "ua.itaysonlab.messenger",
+            "org.forkclient.messenger",
+            "org.forkclient.messenger.beta",
+            "org.aka.messenger",
+            "ellipi.messenger",
+            "org.nift4.catox",
+            "it.owlgram.android",
+            "me.ninjagram.messenger",
+            "ir.ilmili.telegraph");
+
+    @Override
+    public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) {
+        if (hookPackages.contains(lpparam.packageName)) {
+            try {
+                Class<?> messagesControllerClass = XposedHelpers.findClassIfExists("org.telegram.messenger.MessagesController", lpparam.classLoader);
+                if (messagesControllerClass != null) {
+                    XposedBridge.hookAllMethods(messagesControllerClass, "getSponsoredMessages", XC_MethodReplacement.returnConstant(null));
+                    XposedBridge.hookAllMethods(messagesControllerClass, "isChatNoForwards", XC_MethodReplacement.returnConstant(false));
+                }
+                Class<?> chatUIActivityClass = XposedHelpers.findClassIfExists("org.telegram.ui.ChatActivity", lpparam.classLoader);
+                if (chatUIActivityClass != null) {
+                    XposedBridge.hookAllMethods(chatUIActivityClass, "addSponsoredMessages", XC_MethodReplacement.returnConstant(null));
+                }
+                Class<?> SharedConfigClass = XposedHelpers.findClassIfExists("org.telegram.messenger.SharedConfig", lpparam.classLoader);
+                if (SharedConfigClass != null) {
+                    XposedBridge.hookAllMethods(SharedConfigClass, "getDevicePerformanceClass", XC_MethodReplacement.returnConstant(2));
+                }
+                Class<?> UserConfigClass = XposedHelpers.findClassIfExists("org.telegram.messenger.UserConfig", lpparam.classLoader);
+                if (UserConfigClass != null) {
+                    XposedBridge.hookAllMethods(UserConfigClass, "getMaxAccountCount", XC_MethodReplacement.returnConstant(999));
+                    XposedBridge.hookAllMethods(UserConfigClass, "isPremium", XC_MethodReplacement.returnConstant(true));
+                }
+                if (hookPackages.contains("ir.ilmili.telegraph")) {
+                    Class<?> DonateTelegraph = XposedHelpers.findClassIfExists("org.telegram.tgnet.ConnectionsManager", lpparam.classLoader);
+                    if (DonateTelegraph != null) {
+                        XposedBridge.hookAllMethods(DonateTelegraph, "native_expireFile", XC_MethodReplacement.returnConstant(false));
+                        XposedBridge.hookAllMethods(DonateTelegraph, "native_daysFile", XC_MethodReplacement.returnConstant(999));
+                        XposedBridge.hookAllMethods(DonateTelegraph, "native_removeInstance", XC_MethodReplacement.returnConstant(true));
+                        XposedBridge.hookAllMethods(DonateTelegraph, "native_checkLicense", XC_MethodReplacement.returnConstant(true));
+                    }
+                    Class<?> SponsoredTelegraph = XposedHelpers.findClassIfExists("org.telegram.ui.bj", lpparam.classLoader);
+                    if (SponsoredTelegraph != null) {
+                        XposedBridge.hookAllMethods(SponsoredTelegraph, "Jn", XC_MethodReplacement.returnConstant(null));
+                    }
+                    Class<?> MessagesControllerTelegraph = XposedHelpers.findClassIfExists("org.telegram.messenger.m50", lpparam.classLoader);
+                    if (MessagesControllerTelegraph != null) {
+                        XposedBridge.hookAllMethods(MessagesControllerTelegraph, "M8", XC_MethodReplacement.returnConstant(false));
+                    }
+                    Class<?> UserConfigTelegraph = XposedHelpers.findClassIfExists("org.telegram.messenger.go0", lpparam.classLoader);
+                    if (UserConfigTelegraph != null) {
+                        XposedBridge.hookAllMethods(UserConfigTelegraph, "G", XC_MethodReplacement.returnConstant(true));
+                    }
+                }
+            } catch (Throwable ignored) {
+            }
+        }
+    }
+}
