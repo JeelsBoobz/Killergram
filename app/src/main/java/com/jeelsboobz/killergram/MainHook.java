924bd5b10d0a3deda31383ab469db6b8e42f7bc8
JeelsBoobz <bologok.corp@gmail.com>
2022-12-14 22:08:54 +0700

    Update to support Telegraph vT9.1.6 - P10.4

--- app/src/main/java/com/jeelsboobz/killergram/MainHook.java
+++ app/src/main/java/com/jeelsboobz/killergram/MainHook.java
@@ -59,15 +59,15 @@ public class MainHook implements IXposedHookLoadPackage {
                         XposedBridge.hookAllMethods(DonateTelegraph, "native_removeInstance", XC_MethodReplacement.returnConstant(true));
                         XposedBridge.hookAllMethods(DonateTelegraph, "native_checkLicense", XC_MethodReplacement.returnConstant(true));
                     }
-                    Class<?> SponsoredTelegraph = XposedHelpers.findClassIfExists("org.telegram.ui.fj", lpparam.classLoader);
+                    Class<?> SponsoredTelegraph = XposedHelpers.findClassIfExists("org.telegram.ui.bj", lpparam.classLoader);
                     if (SponsoredTelegraph != null) {
-                        XposedBridge.hookAllMethods(SponsoredTelegraph, "Wn", XC_MethodReplacement.returnConstant(null));
+                        XposedBridge.hookAllMethods(SponsoredTelegraph, "vp", XC_MethodReplacement.returnConstant(null));
                     }
-                    Class<?> MessagesControllerTelegraph = XposedHelpers.findClassIfExists("org.telegram.messenger.m60", lpparam.classLoader);
+                    Class<?> MessagesControllerTelegraph = XposedHelpers.findClassIfExists("org.telegram.messenger.g70", lpparam.classLoader);
                     if (MessagesControllerTelegraph != null) {
-                        XposedBridge.hookAllMethods(MessagesControllerTelegraph, "P8", XC_MethodReplacement.returnConstant(false));
+                        XposedBridge.hookAllMethods(MessagesControllerTelegraph, "c9", XC_MethodReplacement.returnConstant(false));
                     }
-                    Class<?> UserConfigTelegraph = XposedHelpers.findClassIfExists("org.telegram.messenger.hp0", lpparam.classLoader);
+                    Class<?> UserConfigTelegraph = XposedHelpers.findClassIfExists("org.telegram.messenger.mr0", lpparam.classLoader);
                     if (UserConfigTelegraph != null) {
                         XposedBridge.hookAllMethods(UserConfigTelegraph, "H", XC_MethodReplacement.returnConstant(true));
                     }
