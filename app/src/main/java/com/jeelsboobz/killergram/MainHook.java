74db456266628912da08cd761f05cbc8b55fe696
JeelsBoobz <bologok.corp@gmail.com>
2023-03-13 21:41:35 +0700

    Update to support Telegraph T9.4.9 - P10.7

--- app/src/main/java/com/jeelsboobz/killergram/MainHook.java
+++ app/src/main/java/com/jeelsboobz/killergram/MainHook.java
@@ -59,13 +59,13 @@ public class MainHook implements IXposedHookLoadPackage {
                         XposedBridge.hookAllMethods(DonateTelegraph, "native_removeInstance", XC_MethodReplacement.returnConstant(true));
                         XposedBridge.hookAllMethods(DonateTelegraph, "native_checkLicense", XC_MethodReplacement.returnConstant(true));
                     }
-                    Class<?> MessagesControllerTelegraph = XposedHelpers.findClassIfExists("org.telegram.messenger.r70", lpparam.classLoader);
+                    Class<?> MessagesControllerTelegraph = XposedHelpers.findClassIfExists("org.telegram.messenger.m80", lpparam.classLoader);
                     if (MessagesControllerTelegraph != null) {
-                        XposedBridge.hookAllMethods(MessagesControllerTelegraph, "m9", XC_MethodReplacement.returnConstant(false));
+                        XposedBridge.hookAllMethods(MessagesControllerTelegraph, "q9", XC_MethodReplacement.returnConstant(false));
                     }
-                    Class<?> UserConfigTelegraph = XposedHelpers.findClassIfExists("org.telegram.messenger.ss0", lpparam.classLoader);
+                    Class<?> UserConfigTelegraph = XposedHelpers.findClassIfExists("org.telegram.messenger.cu0", lpparam.classLoader);
                     if (UserConfigTelegraph != null) {
-                        XposedBridge.hookAllMethods(UserConfigTelegraph, "K", XC_MethodReplacement.returnConstant(true));
+                        XposedBridge.hookAllMethods(UserConfigTelegraph, "L", XC_MethodReplacement.returnConstant(true));
                     }
                 }
             } catch (Throwable ignored) {
