419b744b3829e723bde7aa31bde30d00c9f0b444
JeelsBoobz <bologok.corp@gmail.com>
2023-04-03 17:09:51 +0700

    Fix code for  Telegraph T9.5.4 - P10.8

--- app/src/main/java/com/jeelsboobz/killergram/MainHook.java
+++ app/src/main/java/com/jeelsboobz/killergram/MainHook.java
@@ -59,9 +59,13 @@ public class MainHook implements IXposedHookLoadPackage {
                         XposedBridge.hookAllMethods(DonateTelegraph, "native_removeInstance", XC_MethodReplacement.returnConstant(true));
                         XposedBridge.hookAllMethods(DonateTelegraph, "native_checkLicense", XC_MethodReplacement.returnConstant(true));
                     }
-                    Class<?> MessagesControllerTelegraph = XposedHelpers.findClassIfExists("org.telegram.messenger.q80", lpparam.classLoader);
+                    Class<?> MessagesControllerTelegraph = XposedHelpers.findClassIfExists("org.telegram.messenger.q90", lpparam.classLoader);
                     if (MessagesControllerTelegraph != null) {
                         XposedBridge.hookAllMethods(MessagesControllerTelegraph, "B9", XC_MethodReplacement.returnConstant(false));
+                    }
+                    Class<?> MessagesControllerTelegraph2 = XposedHelpers.findClassIfExists("org.telegram.messenger.iu", lpparam.classLoader);
+                    if (MessagesControllerTelegraph2 != null) {
+                        XposedBridge.hookAllMethods(MessagesControllerTelegraph2, "B", XC_MethodReplacement.returnConstant(false));
                     }
                     Class<?> UserConfigTelegraph = XposedHelpers.findClassIfExists("org.telegram.messenger.gv0", lpparam.classLoader);
                     if (UserConfigTelegraph != null) {
