114bb53da9e554f17c5bb19b92d04ba18cf85bec
JeelsBoobz <jeelsboobz@gmail.com>
2023-08-27 13:55:33 +0700

    Update to support Telegraph T10.0.1 - P11.1.1M

--- app/src/main/java/com/jeelsboobz/killergram/MainHook.java
+++ app/src/main/java/com/jeelsboobz/killergram/MainHook.java
@@ -59,15 +59,15 @@ public class MainHook implements IXposedHookLoadPackage {
                         XposedBridge.hookAllMethods(DonateTelegraph, "native_removeInstance", XC_MethodReplacement.returnConstant(true));
                         XposedBridge.hookAllMethods(DonateTelegraph, "native_checkLicense", XC_MethodReplacement.returnConstant(true));
                     }
-                    Class<?> MessagesControllerTelegraph = XposedHelpers.findClassIfExists("org.telegram.messenger.cb0", lpparam.classLoader);
+                    Class<?> MessagesControllerTelegraph = XposedHelpers.findClassIfExists("org.telegram.messenger.db0", lpparam.classLoader);
                     if (MessagesControllerTelegraph != null) {
-                        XposedBridge.hookAllMethods(MessagesControllerTelegraph, "ca", XC_MethodReplacement.returnConstant(false));
+                        XposedBridge.hookAllMethods(MessagesControllerTelegraph, "da", XC_MethodReplacement.returnConstant(false));
                     }
                     Class<?> MessagesControllerTelegraph2 = XposedHelpers.findClassIfExists("org.telegram.messenger.gv", lpparam.classLoader);
                     if (MessagesControllerTelegraph2 != null) {
                         XposedBridge.hookAllMethods(MessagesControllerTelegraph2, "D", XC_MethodReplacement.returnConstant(false));
                     }
-                    Class<?> UserConfigTelegraph = XposedHelpers.findClassIfExists("org.telegram.messenger.by0", lpparam.classLoader);
+                    Class<?> UserConfigTelegraph = XposedHelpers.findClassIfExists("org.telegram.messenger.cy0", lpparam.classLoader);
                     if (UserConfigTelegraph != null) {
                         XposedBridge.hookAllMethods(UserConfigTelegraph, "N", XC_MethodReplacement.returnConstant(true));
                     }
